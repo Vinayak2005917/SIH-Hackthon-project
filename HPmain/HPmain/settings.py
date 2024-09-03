0a3dbab05defaa228447c995cea4fb78ddc01a4a
Vinayak <vk2005917@gmail.com>
2024-09-03 18:41:03 +0530

    debug

--- HPmain/HPmain/settings.py
+++ HPmain/HPmain/settings.py
@@ -37,10 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main',
+    'test',
     'admin',
     'student',
     'teacher',
+    ''
 ]
 
 MIDDLEWARE = [
--- HPmain/Test/urls.py
+++ HPmain/Test/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("main/", views.home, name='home')
+    path("test/", views.home, name='home')
 
 ]
