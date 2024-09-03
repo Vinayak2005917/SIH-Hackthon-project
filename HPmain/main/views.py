474562f12756624f8515c62c8ab35750902722d0
Vinayak <vk2005917@gmail.com>
2024-09-03 18:16:39 +0530

    add explaintiory text to main file

--- HPmain/main/views.py
+++ HPmain/main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Test 3 (Git commit test!)")
+    return HttpResponse("This is the main one, used to do tests or other stuff you might not wanna do on the others")
