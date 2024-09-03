b2ecf12ee8789becd7178ad27faf362055a205ba
Vinayak <vk2005917@gmail.com>
2024-09-03 18:21:55 +0530

    added admin, student and techer files

--- HPmain/HPmain/settings.py
+++ HPmain/HPmain/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
+    'main',
+    'admin',
+    'student',
+    'teacher',
 ]
 
 MIDDLEWARE = [
