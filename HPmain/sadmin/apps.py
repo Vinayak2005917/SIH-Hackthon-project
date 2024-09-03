7c4ddcf9bd350a8ab05b378ec16caef9842e76ae
Vinayak <vk2005917@gmail.com>
2024-09-03 21:06:20 +0530

    full reset

---
+++ HPmain/sadmin/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SadminConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'sadmin'
---
+++ HPmain/testing/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render, HttpResponse
+
+def testinfo(request):
+    return HttpResponse("This is testing page.")
