54301336a9083ff8c5462f50d5d666a315f9ecbe
Vinayak <vk2005917@gmail.com>
2024-09-05 02:21:33 +0530

    removed the sql db problem

--- HPmain/HPmain/settings.py
+++ HPmain/HPmain/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'HPmain.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME' : 'HPmainDB',
-        'USER' : "root",
-        'PASSWORD' : "axon9876",
-        'HOST' : 'localhost',
-        "PORT" : '3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME' : BASE_DIR / 'db.sqlite3'
     }
 }
 
