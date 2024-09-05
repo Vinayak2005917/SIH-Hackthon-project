691c249cfac8825ccf538880187c9799143af737
Vinayak <vk2005917@gmail.com>
2024-09-05 22:51:31 +0530

    (broken) added a plotform for file upload. all of backend done

--- HPmain/teacher/urls.py
+++ HPmain/teacher/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.teachinfo, name = 'teachinfo')
+    path("", views.teachinfo, name = 'teachinfo'),
+    path('upload/', views.upload_file, name='upload_file'),
+    path('upload_success/', views.upload_success, name='upload_success'),
 ]
--- HPmain/teacher/views.py
+++ HPmain/teacher/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, redirect
+from .forms import UploadFileForm
 
 def teachinfo(request):
+    print("teacher.html loaded")
     return render(request, 'teacher.html')
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('upload_success')
+    else:
+        form = UploadFileForm()
+    return render(request, 'upload.html', {'form': form})
+
+def upload_success(request):
+    return render(request, 'upload_success.html')
