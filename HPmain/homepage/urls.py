a918d445c46db931e571d6237809db5ef034c4d6
Vinayak <vk2005917@gmail.com>
2024-09-08 02:12:09 +0530

    added the contributors page

--- HPmain/homepage/urls.py
+++ HPmain/homepage/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name = 'home'),
+    path('credits/', views.credits, name='credits'),
 ]
--- HPmain/homepage/views.py
+++ HPmain/homepage/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home page.html')
+
+def credits(request):
+    return render(request, 'credit main.html')
